2a69ec98c80a0340699cbd48266c1bc9c6125af1
Tim <hounddog@gmail.com>
2022-07-22 12:35:08 -0400

    init

---
+++ Cli/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using CommandLine;
+using YadaYada.BuildTools.Cli;
+
+Console.WriteLine("Hello, World!");
+
+Parser.Default.ParseArguments<UpdateProjectReferencesCommand>(args)
+    .WithParsed<UpdateProjectReferencesCommand>(o =>
+    {
+        Console.WriteLine($"Update {o.ProjectPath}");
+    });
---
+++ Cli/UpdateProjectReferencesCommand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommandLine;
+
+namespace YadaYada.BuildTools.Cli
+{
+    public class UpdateProjectReferencesCommand
+    {
+        [Option('p', "project", Required = true, HelpText = "Project Path")]
+        public string ProjectPath { get; set; }
+    }
+}
