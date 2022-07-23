950d0a49533e8afed23fe9ef9fc9a742e2d84415
Tim <hounddog@gmail.com>
2022-07-23 13:27:25 -0400

    C:\Users\17034\source\repos\bbmono\yadayadalibrary\Extensions\StackStatusExtensions.cs

---
+++ Amazon.CloudFormation/CloudFormation/StackStatusExtensions.cs
@@ -0,0 +1,22 @@
+﻿namespace Amazon.CloudFormation;
+
+public static class StackStatusExtensions
+{
+    public static bool IsActive(this StackStatus status)
+    {
+        return status == StackStatus.CREATE_COMPLETE 
+               || status == StackStatus.IMPORT_COMPLETE 
+               || status == StackStatus.UPDATE_COMPLETE 
+               || status == StackStatus.UPDATE_ROLLBACK_COMPLETE ;
+    }
+
+    public static bool IsRollingBack(this StackStatus status)
+    {
+        return status == StackStatus.ROLLBACK_IN_PROGRESS || status == StackStatus.UPDATE_ROLLBACK_IN_PROGRESS || status == StackStatus.UPDATE_ROLLBACK_COMPLETE_CLEANUP_IN_PROGRESS;
+    }
+    public static bool IsCleaningUp(this StackStatus status)
+    {
+        return status == StackStatus.UPDATE_COMPLETE_CLEANUP_IN_PROGRESS;
+    }
+
+}
