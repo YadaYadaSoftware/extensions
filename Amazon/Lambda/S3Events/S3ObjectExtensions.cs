bd9dd83cda0f79d2ca3aef4bb057dd74b8fbab14
Tim <hounddog@gmail.com>
2022-07-23 08:05:40 -0400

    S3ObjectExtensions

---
+++ Amazon/Lambda/S3Events/S3ObjectExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// ReSharper disable once CheckNamespace
+namespace Amazon.S3.Model;
+
+public static class S3ObjectExtensions
+{
+    public static bool IsXml(this S3Object s3Object)
+    {
+        return Path.GetExtension(s3Object.Key).Equals(".xml", StringComparison.InvariantCultureIgnoreCase);
+    }
+}
+
