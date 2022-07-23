229b517e08b1c3e0522fd6cfea8376ab27a050dd
Tim <hounddog@gmail.com>
2022-07-23 07:42:09 -0400

    s3events

---
+++ Amazon/Lambda/S3Events/LoggerExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Amazon.S3.Model;
+
+namespace Microsoft.Extensions.Logging;
+
+public static class LoggerExtensions
+{
+    public static IDisposable AddScope(this ILogger logger, S3Object s3Object)
+    {
+        AggregateScope aggregateScope = new()
+        {
+            Capacity = 2
+        };
+        aggregateScope.Add(logger.AddScope($"{nameof(S3Object)}.{nameof(s3Object.BucketName)}", s3Object.BucketName));
+        aggregateScope.Add(logger.AddScope($"{nameof(S3Object)}.{nameof(s3Object.Key)}", s3Object.Key));
+
+        return aggregateScope;
+
+    }
+
+}
