32095b66833f03365e54cf0be64b7bd3ab1f0886
Tim <hounddog@gmail.com>
2022-07-23 13:34:11 -0400

    string extension

---
+++ System/StringExtension.cs
@@ -0,0 +1,16 @@
+﻿namespace System;
+
+public static class StringExtension
+{
+    public static Guid ToGuid(this string value)
+    {
+        return Guid.Parse(value);
+    }
+
+    public static string ToAscii(this string source)
+    {
+        var min = '\u0000';
+        var max = '\u007F';
+        return new string(source.Where(c => c >= min && c <= max).ToArray());
+    }
+}
