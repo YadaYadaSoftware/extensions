5571dbc0003faac7111af0a71774c3a5e6207d3b
Tim <hounddog@gmail.com>
2022-07-23 13:12:53 -0400

    namespace Microsoft.Extensions.DependencyInjection

---
+++ Microsoft.Extensions.DependencyInjection/IServiceCollectionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    // ReSharper disable once InconsistentNaming
+    public static class IServiceCollectionExtensions
+    {
+        public static IServiceCollection TryAddEnumerableScoped<TService, TImplementation>(this IServiceCollection serviceCollection) where TService : class where TImplementation : class, TService
+        {
+            serviceCollection.TryAddEnumerable(new ServiceDescriptor(typeof(TService), typeof(TImplementation), ServiceLifetime.Scoped));
+            return serviceCollection;
+        }
+        public static IServiceCollection TryAddEnumerableSingleton<TService, TImplementation>(this IServiceCollection serviceCollection) where TService : class where TImplementation : class, TService
+        {
+            serviceCollection.TryAddEnumerable(new ServiceDescriptor(typeof(TService), typeof(TImplementation), ServiceLifetime.Singleton));
+            return serviceCollection;
+        }
+    }
+}
