4546488f0c84d0156660c3659dd39a0728f4f1a7
Tim <hounddog@gmail.com>
2022-07-23 12:50:31 -0400

    some tests

---
+++ System.Reflection.Test/TypeExtensionsTest.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using FluentAssertions;
+
+namespace System.Reflection;
+
+public class TypeExtensionsTest
+{
+    [Fact]
+    public void GetGroupNamesTest()
+    {
+        var groupNames = typeof(SampleEntity).GetGroupNames();
+        groupNames.First().Should().Be("Group 1");
+        groupNames.Last().Should().Be("Group 2");
+
+    }
+
+    [Fact]
+    public void HasPropertyTest()
+    {
+        var t = typeof(SampleEntity);
+        t.HasProperty(nameof(SampleEntity.Name)).Should().BeTrue();
+    }
+
+
+}
+
+[Display(
+    ResourceType = typeof(Properties.SampleEntity.Class), 
+    GroupName = nameof(DisplayAttribute.GroupName))]
+public class SampleEntity
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
