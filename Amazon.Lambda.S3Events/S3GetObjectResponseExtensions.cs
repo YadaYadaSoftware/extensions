a77d966fe10d7249f6ed6a244b84c60db9039e01
Tim <hounddog@gmail.com>
2022-07-23 13:20:06 -0400

    S3GetObjectResponseExtensions

---
+++ Amazon.Lambda.S3Events/S3GetObjectResponseExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Amazon.S3.Model;
+
+namespace Amazon.Lambda.S3Events;
+
+public static class S3GetObjectResponseExtensions
+{
+    public static bool TryGetMetadata(this GetObjectResponse getObjectResponse, string key, out Guid result)
+    {
+        result = default;
+        return Guid.TryParse(getObjectResponse.Metadata[key], out result);
+    }
+
+    public static bool TryGetMetadata(this GetObjectResponse getObjectResponse, string key, out string result)
+    {
+        result = getObjectResponse.Metadata[key];
+        return !string.IsNullOrEmpty(result);
+    }
+}
