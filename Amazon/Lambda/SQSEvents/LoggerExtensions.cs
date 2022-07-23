d628d8b2e1dc39686a9723ba790e9b680ef96d08
Tim <hounddog@gmail.com>
2022-07-23 07:16:46 -0400

    sqsevents

---
+++ Amazon/Lambda/SQSEvents/LoggerExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Amazon.Lambda.SQSEvents;
+
+namespace Microsoft.Extensions.Logging;
+
+public static class LoggerExtensions
+{
+    public static IDisposable AddScope(this ILogger logger, SQSEvent.SQSMessage message)
+    {
+        AggregateScope aggregateScope = new()
+        {
+            Capacity = message.MessageAttributes.Count + 1
+        };
+        aggregateScope.Add(logger.AddScope(nameof(message.MessageId), message.MessageId));
+
+        foreach (var (key, value) in message.MessageAttributes)
+        {
+            aggregateScope.Add(logger.AddScope(key, value.StringValue));
+        }
+
+        return aggregateScope;
+    }
+
+}
