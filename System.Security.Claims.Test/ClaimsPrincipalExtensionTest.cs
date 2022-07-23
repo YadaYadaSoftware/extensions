2733ba2c1d91882fbd55141a103d1c6d81172452
Tim <hounddog@gmail.com>
2022-07-23 11:56:54 -0400

    added more test

---
+++ System.Security.Claims.Test/ClaimsPrincipalExtensionTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Xunit;
+
+namespace YadaYada.Library.Extensions;
+
+public class ClaimsPrincipalExtensionTest
+{
+    [Fact]
+    public void AddTwoClaims()
+    {
+        var serviceCollection = new ServiceCollection();
+        var loggerProvider = new Mock<ILoggerProvider>();
+        loggerProvider.Setup(_ => _.CreateLogger(It.IsAny<string>())).Returns(new Mock<ILogger>().Object);
+        serviceCollection.AddSingleton<ILoggerProvider>(loggerProvider.Object);
+        serviceCollection.AddClaim(ClaimsExtensions.ClaimTypes.TenantId, Guid.NewGuid().ToString());
+        serviceCollection.AddClaim(ClaimsExtensions.ClaimTypes.AmazonSellerId, Guid.NewGuid().ToString());
+        serviceCollection.Where(_ => _.ServiceType == typeof(Claim)).Should().HaveCount(2);
+        var provider = serviceCollection.BuildServiceProvider();
+    }
+}
