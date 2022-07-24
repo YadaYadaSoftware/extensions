213346b2eeb4c9398c8ec2b48b6e77b5f75ca505
Tim <hounddog@gmail.com>
2022-07-24 13:11:48 -0400

    tests

---
+++ Amazon.Lambda.S3Events.Test/GetObjectResponseExtensionTest.cs
@@ -0,0 +1,22 @@
+﻿using Amazon.Lambda.S3Events;
+using Amazon.S3.Model;
+using FluentAssertions;
+using Xunit;
+
+namespace YadaYada.Lambda
+{
+    public class GetObjectResponseExtensionTest
+    {
+        [Fact]
+        public void TryGetUserAgentTest()
+        {
+            GetObjectResponse response = new GetObjectResponse();
+            
+            response.TryGetUserAgent(out string userAgent).Should().BeFalse();
+            const string myuseragent = "myuseragent";
+            response.Metadata.Add(S3GetObjectResponseExtensions.Constants.MetadataKeys.UserAgent, myuseragent);
+            response.TryGetUserAgent(out userAgent).Should().BeTrue();
+            userAgent.Should().Be(myuseragent);
+        }
+    }
+}
