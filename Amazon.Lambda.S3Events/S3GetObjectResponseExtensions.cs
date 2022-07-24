b101428b2d08a557ca3cf3b23ee951d37c781382
Tim <hounddog@gmail.com>
2022-07-24 04:32:48 -0400

    S3GetObjectResponseExtensions

--- Amazon.Lambda.S3Events/S3GetObjectResponseExtensions.cs
+++ Amazon.Lambda.S3Events/S3GetObjectResponseExtensions.cs
@@ -4,6 +4,22 @@ namespace Amazon.Lambda.S3Events;
 
 public static class S3GetObjectResponseExtensions
 {
+    public static class Constants
+    {
+        public static class MetadataKeys
+        {
+            //x-amz-meta-user-agent-id
+            public const string UserAgent = "user-agent-id";
+        }
+    }
+
+    public static string GetExtension(this GetObjectResponse getObjectResponse) => Path.GetExtension(getObjectResponse.Key.ToLowerInvariant());
+
+    public static bool TryGetUserAgent(this GetObjectResponse getObjectResponse, out string userAgent)
+    {
+        userAgent = getObjectResponse.Metadata[Constants.MetadataKeys.UserAgent]?.Split('@').First();
+        return !string.IsNullOrEmpty(userAgent);
+    }
     public static bool TryGetMetadata(this GetObjectResponse getObjectResponse, string key, out Guid result)
     {
         result = default;
