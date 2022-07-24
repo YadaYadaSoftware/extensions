7caa198e87813fcc8694cf1f5481150f8d73e21c
Tim <hounddog@gmail.com>
2022-07-24 13:05:26 -0400

    C:\Users\17034\source\repos\bbmono\yadayadalibrary\Lambda.Tests\Services\EnvironmentVariableSettingServiceTest.cs

---
+++ Amazon.Lambda.Core.Tests/EnvironmentVariableSettingServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using FluentAssertions;
+
+// ReSharper disable once CheckNamespace
+namespace Microsoft.Extensions.DependencyInjection;
+
+public class EnvironmentVariableSettingServiceTest
+{
+    [Fact]
+    public void NullThrows()
+    {
+        var t = new EnvironmentVariableSettingService();
+        // ReSharper disable once AssignNullToNotNullAttribute
+        t.Invoking(_ => _.GetAsync(null)).Should().ThrowAsync<ArgumentNullException>();
+        t.Invoking(_ => _.GetAsync(string.Empty)).Should().ThrowAsync<ArgumentNullException>();
+        t.Invoking(_ => _.GetAsync(Guid.NewGuid().ToString())).Should().ThrowAsync<KeyNotFoundException>();
+    }
+
+}
