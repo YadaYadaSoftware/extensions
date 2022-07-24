11b04439bbba62d50973212921b468933b82d50f
Tim <hounddog@gmail.com>
2022-07-24 04:41:08 -0400

    C:\Users\17034\source\repos\bbmono\yadayadalibrary\Lambda\IS3EventHandler.cs

---
+++ Amazon.Lambda.S3Events/IS3EventHandler.cs
@@ -0,0 +1,10 @@
+﻿using Amazon.Lambda.Core;
+using Amazon.S3.Model;
+
+namespace Amazon.Lambda.S3Events;
+
+public interface IS3EventHandler
+{
+    Task HandleNewS3ObjectAsync(Guid eventId, S3Object s3Object, ILambdaContext lambdaContext);
+    Task<bool> CanHandleAsync(S3Object s3Object, ILambdaContext lambdaContext);
+}
