d909ca07744a1acbed9a26b6d60646fa2f97fa75
Tim <hounddog@gmail.com>
2022-07-24 04:03:07 -0400

    SqsMessageExtensions

---
+++ Amazon.Lambda.SQSEvents/SqsMessageExtensions.cs
@@ -0,0 +1,47 @@
+﻿// ReSharper disable once CheckNamespace
+namespace Amazon.Lambda.SQSEvents;
+
+public static class SqsMessageExtensions
+{
+
+    //public static class Constants
+    //{
+    //    public static class MessageAttributes
+    //    {
+    //        public const string TenantIdKey = "TenantId";
+    //        public const string MessageType = "MessageType";
+    //        public const string EventId = "EventId";
+    //    }
+    //}
+    public static bool TryGetTenantId(this SQSEvent.SQSMessage message, out Guid tenantId)
+    {
+        tenantId = default;
+
+        return message.MessageAttributes.SingleOrDefault(_ => _.Key == nameof(TenantBasedMessage.TenantId)) is {Value: { } value}
+               && Guid.TryParse(value.StringValue, out tenantId);
+    }
+
+
+    public static bool TryGetHandlerType(this SQSEvent.SQSMessage message, out Type type)
+    {
+        type = default;
+        if (message.MessageAttributes.SingleOrDefault(_ => _.Key == nameof(TenantBasedMessage.MessageType)) is not {Value: { } value}) return false;
+        type = Type.GetType(value.StringValue);
+        return type != default;
+    }
+    public static bool TryGetEventId(this SQSEvent.SQSMessage message, out Guid eventId)
+    {
+        eventId = default;
+
+        return (message.MessageAttributes.SingleOrDefault(_ => _.Key == nameof(TenantBasedMessage.EventId)) is
+            {Value: { } value} && Guid.TryParse(value.StringValue, out eventId));
+    }
+
+    public static bool TryGetFileProcessingId(this SQSEvent.SQSMessage message, out Guid fileProcessingId)
+    {
+        fileProcessingId = Guid.Empty;
+        return message.MessageAttributes.SingleOrDefault(_=>_.Key == TenantBasedMessage.FileProcessingId) is {Value:{} value}  && Guid.TryParse(value.StringValue, out fileProcessingId);
+    }
+
+
+}
