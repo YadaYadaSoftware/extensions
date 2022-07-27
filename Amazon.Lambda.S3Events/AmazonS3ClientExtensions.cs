ba8ebe4d37a17fc2e3081cac23438b664fbc9565
Tim <hounddog@gmail.com>
2022-07-27 11:48:43 -0400

    tasks and logging

--- Amazon.Lambda.S3Events/AmazonS3ClientExtensions.cs
+++ Amazon.Lambda.S3Events/AmazonS3ClientExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using Amazon.S3;
 using Amazon.S3.Model;
+using Microsoft.Extensions.Logging;
 
 namespace Amazon.Lambda.S3Events;
 
@@ -9,16 +10,82 @@ public static class AmazonS3ClientExtensions
         string sourceFolder,
         string destinationBucket,
         string destinationFolder,
-        CancellationToken cancellationToken = default(CancellationToken))
+        CancellationToken cancellationToken = default(CancellationToken),
+        ILogger? logger = default)
     {
-        var listObjectsV2Response = await amazonS3.ListObjectsV2Async(
-            new ListObjectsV2Request{BucketName = sourceBucket, Prefix = sourceFolder}, 
-            cancellationToken);
 
-        listObjectsV2Response.S3Objects.ForEach(o =>
+        AggregateScope? loggerAggregateScope = null;
+
+        if (logger != null)
         {
-            var destinationKey = destinationFolder + o.Key.Replace(sourceFolder, string.Empty);
-            amazonS3.CopyObjectAsync(o.BucketName, o.Key, destinationBucket, destinationKey, cancellationToken);
-        });
+            loggerAggregateScope = new AggregateScope();
+            loggerAggregateScope.Add(logger.AddMember());
+            loggerAggregateScope.Add(logger.AddScope(nameof(sourceBucket), sourceBucket));
+            loggerAggregateScope.Add(logger.AddScope(nameof(sourceFolder), sourceFolder));
+            loggerAggregateScope.Add(logger.AddScope(nameof(destinationBucket), destinationBucket));
+            loggerAggregateScope.Add(logger.AddScope(nameof(destinationFolder), destinationFolder));
+
+        }
+
+        try
+        {
+            var listObjectsV2Response = await amazonS3.ListObjectsV2Async(
+                new ListObjectsV2Request {BucketName = sourceBucket, Prefix = sourceFolder},
+                cancellationToken);
+
+            var tasks = new List<Task>();
+
+            Task[] incompleteTasks;
+
+            listObjectsV2Response.S3Objects.ForEach(async o =>
+            {
+                var addScopeKey = logger?.AddScope(nameof(o.Key), o.Key);
+                loggerAggregateScope?.Add(addScopeKey);
+                var destinationKey = destinationFolder + o.Key.Replace(sourceFolder, string.Empty);
+                var addScopeDestinationKey = logger?.AddScope(nameof(destinationKey), destinationKey);
+                loggerAggregateScope?.Add(addScopeDestinationKey);
+
+                try
+                {
+                    tasks.Add(amazonS3.CopyObjectAsync(o.BucketName, o.Key, destinationBucket, destinationKey, cancellationToken));
+
+                    incompleteTasks = tasks.Where(_ => !_.IsCompleted).ToArray();
+                    while (incompleteTasks.Length > 20)
+                    {
+                        await Task.WhenAny(incompleteTasks);
+                        incompleteTasks = tasks.Where(_ => !_.IsCompleted).ToArray();
+                    }
+
+                }
+                catch (Exception e)
+                {
+                    logger?.LogError(e, e.ToString());
+
+                    throw;
+                }
+                finally
+                {
+                    loggerAggregateScope?.Remove(addScopeKey);
+                    addScopeKey?.Dispose();
+                    loggerAggregateScope?.Remove(addScopeDestinationKey);
+                    addScopeDestinationKey?.Dispose();
+                }
+            });
+
+            incompleteTasks = tasks.Where(_ => !_.IsCompleted).ToArray();
+
+            await Task.WhenAll(incompleteTasks);
+
+        }
+        catch (Exception e)
+        {
+            logger?.LogError(e, e.ToString());
+
+            throw;
+        }
+        finally
+        {
+            loggerAggregateScope?.Dispose();
+        }
     }
 }
