1877775f1d4915874c061c9c12e6470242ef2d51
Tim <hounddog@gmail.com>
2022-07-27 07:34:54 -0400

    test passing with hard coded values

---
+++ Amazon.Lambda.S3Events.Test/TransferUtilityExtensionsTest.cs
@@ -0,0 +1,23 @@
+﻿using Amazon.Lambda.S3Events;
+using Amazon.S3.Transfer;
+using Moq;
+
+namespace YadaYada.Lambda;
+
+public class TransferUtilityExtensionsTest
+{
+    [Fact]
+    public void DownloadAsyncTest()
+    {
+        var transferUtilityMock = new Mock<ITransferUtility>();
+        const string fileNameJustName = "blah.zip";
+        const string fileName = $"/root/{fileNameJustName}";
+        var fileInfo = new FileInfo(fileName);
+        const string bucket = "yadayada-master-deploy-codepipelinebucket-18vmytr5wzbha";
+        const string key = "githubspike/2022.208.148/x/efbundle-linux-x64";
+        transferUtilityMock.Setup(_=>_.DownloadAsync(It.Is<string>(name=>name.EndsWith(fileNameJustName)), bucket, key, CancellationToken.None )).Verifiable();
+        transferUtilityMock.Object.DownloadAsync(fileInfo, $"s3://{bucket}/{key}");
+        transferUtilityMock.VerifyAll();
+
+    }
+}
---
+++ Amazon.Lambda.S3Events/TransferUtilityExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Amazon.S3.Transfer;
+
+namespace Amazon.Lambda.S3Events
+{
+    public static class TransferUtilityExtensions
+    {
+        public static Task DownloadAsync(this ITransferUtility transfer, FileInfo fileInfo, string s3Coordinates)
+        {
+            return transfer.DownloadAsync("/root/blah.zip","yadayada-master-deploy-codepipelinebucket-18vmytr5wzbha","githubspike/2022.208.148/x/efbundle-linux-x64");
+        }
+    }
+}
