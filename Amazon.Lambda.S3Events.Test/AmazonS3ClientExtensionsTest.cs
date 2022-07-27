88a0f9cedd1edae6a9b8e6ae6df93d7752d18dc8
Tim <hounddog@gmail.com>
2022-07-27 10:03:00 -0400

    shell of test

---
+++ Amazon.Lambda.S3Events.Test/AmazonS3ClientExtensionsTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Amazon.Lambda.S3Events;
+using Amazon.S3;
+using Amazon.S3.Model;
+using Moq;
+
+namespace S3Events.Test
+{
+    public class AmazonS3ClientExtensionsTest
+    {
+        [Fact]
+        public async void CopyFolderAsyncTest()
+        {
+            var s3 = new Mock<IAmazonS3>();
+            const string sourceBucket = "sourceBucket";
+
+            var cancellationToken = new CancellationToken();
+            s3.Setup(x => x.ListObjectsV2Async(It.IsAny<ListObjectsV2Request>(), cancellationToken)).Verifiable();
+
+            await s3.Object.CopyFolderAsync(sourceBucket, string.Empty, string.Empty, string.Empty, cancellationToken);
+
+            s3.VerifyAll();
+
+        }
+    }
+}
---
+++ Amazon.Lambda.S3Events/AmazonS3ClientExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Amazon.S3;
+using Amazon.S3.Model;
+
+namespace Amazon.Lambda.S3Events;
+
+public static class AmazonS3ClientExtensions
+{
+    public static async Task CopyFolderAsync(this IAmazonS3 amazonS3, string sourceBucket,
+        string sourceFolder,
+        string destinationBucket,
+        string destinationFolder,
+        CancellationToken cancellationToken = default(CancellationToken))
+    {
+        await amazonS3.ListObjectsV2Async(new ListObjectsV2Request(), cancellationToken);
+    }
+}
