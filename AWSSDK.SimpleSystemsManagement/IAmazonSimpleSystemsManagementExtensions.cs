83d64f3b95628b97cfba5e8ae1d1259efac2d5ec
Tim <hounddog@gmail.com>
2022-07-28 23:12:04 -0400

    ParameterExistsAsync

--- AWSSDK.SimpleSystemsManagement/IAmazonSimpleSystemsManagementExtensions.cs
+++ AWSSDK.SimpleSystemsManagement/IAmazonSimpleSystemsManagementExtensions.cs
@@ -16,8 +16,32 @@ namespace Amazon.SimpleSystemsManagement
         // ReSharper disable once UnusedMember.Global
         public static async Task<bool> ParameterExistsAsync(this IAmazonSimpleSystemsManagement client, string parameterPath)
         {
-            var results = await client.DescribeParametersAsync(new DescribeParametersRequest {ParameterFilters = new List<ParameterStringFilter> {new ParameterStringFilter {Key = "Name", Values = new List<string> {parameterPath}}}});
-            return results.Parameters.Any();
+            try
+            {
+                Console.WriteLine(nameof(ParameterExistsAsync));
+                var describeParametersResponse = await client.DescribeParametersAsync(new()
+                {
+                    ParameterFilters = new() {new()
+            {
+                Key = "Name", Values = new()
+                {
+                    parameterPath
+                }
+            }}
+                });
+                Console.WriteLine($"{nameof(ParameterExistsAsync)}:{nameof(describeParametersResponse)}={describeParametersResponse}");
+
+
+                var parameterExistsAsync = describeParametersResponse.Parameters.Any();
+                Console.WriteLine($"{parameterPath}.Exists={parameterExistsAsync}");
+                return parameterExistsAsync;
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+                throw;
+            }
         }
 
     }
