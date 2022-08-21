a8e13911cda42980bb1ce9a066a78d56641d9569
Tim <hounddog@gmail.com>
2022-08-21 18:34:29 -0400

    NavigationManagerExtensions

---
+++ Components/Class1.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace Components
+{
+    public static class NavigationManagerExtensions
+    {
+        public static ValueTask<object> NavigateTo(this NavigationManager navigationManager, IJSRuntime jsRuntime, string uri, bool newWindow = false)
+        {
+            return jsRuntime.InvokeAsync<object>("open", uri, "_blank");
+        }
+    }
+}
