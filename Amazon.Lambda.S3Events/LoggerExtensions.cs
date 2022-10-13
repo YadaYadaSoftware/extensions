aebc2db7a4f7b8592f8378be0734e5ed4b8254a2
Tim <hounddog@gmail.com>
2022-10-13 19:31:53 -0400

    public static IDisposable AddScope(this ILogger logger, GetObjectResponse s3Object)

--- Amazon.Lambda.S3Events/LoggerExtensions.cs
+++ Amazon.Lambda.S3Events/LoggerExtensions.cs
@@ -18,5 +18,17 @@ public static class LoggerExtensions
         return aggregateScope;
 
     }
+    public static IDisposable AddScope(this ILogger logger, GetObjectResponse s3Object)
+    {
+        AggregateScope aggregateScope = new()
+        {
+            Capacity = 2
+        };
+        aggregateScope.Add(logger.AddScope($"{nameof(S3Object)}.{nameof(s3Object.BucketName)}", s3Object.BucketName));
+        aggregateScope.Add(logger.AddScope($"{nameof(S3Object)}.{nameof(s3Object.Key)}", s3Object.Key));
+
+        return aggregateScope;
+
+    }
 
 }
