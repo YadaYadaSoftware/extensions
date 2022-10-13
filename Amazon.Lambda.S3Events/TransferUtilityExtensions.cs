73513e8403804296d301797f7e63ef16a622cddb
Tim <hounddog@gmail.com>
2022-10-13 19:05:59 -0400

    UploadAsync

--- Amazon.Lambda.S3Events/TransferUtilityExtensions.cs
+++ Amazon.Lambda.S3Events/TransferUtilityExtensions.cs
@@ -3,12 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Amazon.S3.Model;
 using Amazon.S3.Transfer;
 
 namespace Amazon.Lambda.S3Events
 {
     public static class TransferUtilityExtensions
     {
+        public class MetadataKeys
+        {
+            public const string TenantId = "tenantid";
+        }
+
         public static Task DownloadAsync(this ITransferUtility transfer, FileInfo fileInfo, string s3Coordinates)
         {
             string bucket;
@@ -31,6 +37,27 @@ namespace Amazon.Lambda.S3Events
                 throw new NotSupportedException($"The {nameof(s3Coordinates)} of '{s3Coordinates}' is not supported.");
             }
             return transfer.DownloadAsync(fileInfo.FullName, bucket, key);
+        }
+
+        public static Task UploadAsync(this ITransferUtility transferUtility, FileInfo file, string bucketName, string key, Guid tenantId)
+        {
+            if (transferUtility == null) throw new ArgumentNullException(nameof(transferUtility));
+            if (file == null) throw new ArgumentNullException(nameof(file));
+            if (bucketName == null) throw new ArgumentNullException(nameof(bucketName));
+            if (key == null) throw new ArgumentNullException(nameof(key));
+            if (tenantId == Guid.Empty) throw new ArgumentNullException(nameof(tenantId));
+
+            var transferUtilityUploadRequest = new TransferUtilityUploadRequest
+            {
+                Key = key,
+                BucketName = bucketName,
+                FilePath = file.FullName
+            };
+
+            transferUtilityUploadRequest.Metadata.Add(MetadataKeys.TenantId,tenantId.ToString());
+
+            return transferUtility.UploadAsync(transferUtilityUploadRequest);
+
         }
     }
 }
