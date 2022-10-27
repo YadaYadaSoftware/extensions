3574abf0a5ce43f2b57ee62641561a93c53e3541
Tim <hounddog@gmail.com>
2022-10-26 20:29:58 -0400

    fix tests for linuxz

--- Logging.Test/UnitTest1.cs
+++ Logging.Test/UnitTest1.cs
@@ -10,37 +10,37 @@ namespace Logging.Test
         [Fact]
         public void ScopeTest()
         {
-            string expected = "1=2\r {\"CategoryName\":\"LoggerYadaYadaTest\",\"a\":\"1\",\"b\":\"2\"}";
+            string expected = "1=2 {\"CategoryName\":\"LoggerYadaYadaTest\",\"a\":\"1\",\"b\":\"2\"}";
             using var p = new MockPackage<LoggerYadaYada>(CreateLoggerMock);
             string written = String.Empty;
             p.TargetMock.Setup(yada => yada.Write(It.IsAny<string>())).Callback((string s) => written = s);
             p.Target.LogTrace("{a}={b}", 1,2);
-            written.Should().Be(expected);
+            written.Replace("\r", string.Empty) .Should().Be(expected);
         }
 
         [Fact]
         public void BeginScopeTest()
         {
-            string expected = "x\r {\"CategoryName\":\"LoggerYadaYadaTest\",\"Scope\":\"Scope1\"}";
+            string expected = "x {\"CategoryName\":\"LoggerYadaYadaTest\",\"Scope\":\"Scope1\"}";
             using var p = new MockPackage<LoggerYadaYada>(CreateLoggerMock);
             string written = String.Empty;
             p.TargetMock.Setup(yada => yada.Write(It.IsAny<string>())).Callback((string s) => written = s);
             using var scope = p.Target.BeginScope("Scope1");
             p.Target.LogTrace("x");
-            written.Should().Be(expected);
+            written.Replace("\r", string.Empty).Should().Be(expected);
         }
 
         [Fact]
         public void BeginScope2Test()
         {
-            string expected = "x\r {\"CategoryName\":\"LoggerYadaYadaTest\",\"Scope\":\"Scope1, Scope2\"}";
+            string expected = "x {\"CategoryName\":\"LoggerYadaYadaTest\",\"Scope\":\"Scope1, Scope2\"}";
             using var p = new MockPackage<LoggerYadaYada>(CreateLoggerMock);
             string written = String.Empty;
             p.TargetMock.Setup(yada => yada.Write(It.IsAny<string>())).Callback((string s) => written = s);
             using var scope1 = p.Target.BeginScope("Scope1");
             using var scope2 = p.Target.BeginScope("Scope2");
             p.Target.LogTrace("x");
-            written.Should().Be(expected);
+            written.Replace("\r", string.Empty).Should().Be(expected);
         }
 
         private Mock<LoggerYadaYada> CreateLoggerMock()
