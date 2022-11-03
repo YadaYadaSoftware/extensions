b2bd0d1a8ef842bee281ed7e1df809b47691a47c
Tim <hounddog@gmail.com>
2022-11-03 08:57:03 -0400

    DirectoryInfoExtensions

---
+++ System.IO/IO/DirectoryInfoExtensions.cs
@@ -0,0 +1,11 @@
+﻿namespace System.IO;
+
+public static class DirectoryInfoExtensions
+{
+    public static DirectoryInfo Combine(this DirectoryInfo directoryInfo, params string[] paths)
+    {
+        var combine = new List<string> {directoryInfo.FullName};
+        combine.AddRange(paths);
+        return new DirectoryInfo(Path.Combine(combine.ToArray()));
+    }
+}
