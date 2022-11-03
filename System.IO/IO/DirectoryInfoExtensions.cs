da79c28726fb7a9355c57a6fe07719be2fedfb53
Tim <hounddog@gmail.com>
2022-11-03 09:02:15 -0400

    CombineFile

--- System.IO/IO/DirectoryInfoExtensions.cs
+++ System.IO/IO/DirectoryInfoExtensions.cs
@@ -2,10 +2,16 @@
 
 public static class DirectoryInfoExtensions
 {
-    public static DirectoryInfo Combine(this DirectoryInfo directoryInfo, params string[] paths)
+    public static DirectoryInfo CombineDirectory(this DirectoryInfo directoryInfo, params string[] paths)
     {
-        var combine = new List<string> {directoryInfo.FullName};
+        var combine = new List<string> { directoryInfo.FullName };
         combine.AddRange(paths);
         return new DirectoryInfo(Path.Combine(combine.ToArray()));
+    }
+    public static FileInfo CombineFile(this DirectoryInfo directoryInfo, params string[] paths)
+    {
+        var combine = new List<string> { directoryInfo.FullName };
+        combine.AddRange(paths);
+        return new FileInfo(Path.Combine(combine.ToArray()));
     }
 }
